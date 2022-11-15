38dfe8497141935f8349103e95c9fdb110f0d3fa
HoJin9622 <kiss0104040@gmail.com>
2022-11-16 01:39:32 +0900

    #18.3 Dark Mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from './router'
+import theme from './theme'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <ChakraProvider theme={theme}>
       <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
